dd35090e4a5e46b1723420a3eca488eef0b6f80c
leslie-fang <fangjiahao@sjtu.edu.cn>
2017-06-25 13:05:42 +0800

    init

---
+++ FirstNameLastNameHomework5.py
@@ -0,0 +1,16 @@
+
+if __name__ == "__main__":
+    phonebook = {'BOB':'021888881','ALEX':'021888882','LUNA':'021888883',
+                 'JAMES':'021888884','KING':'021888885','HENRY':'021888886'}
+    #for item in phonebook:
+    #    print item
+    #    print phonebook[item]
+
+    print "Welcome to the Python Phonebook"
+    print '''Main Menu
+1. Find a number
+2. Add a person
+3. Remove a person
+4. Change a person's number 5. Reverse lookup'''
+
+
